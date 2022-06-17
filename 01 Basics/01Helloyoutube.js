fdecd5241406ab9a1dcf695f36c36d0a7d3a7ce1
Subodh Singh <sksingh329@gmail.com>
2022-06-17 14:15:38 +0530

    Inital commit

---
+++ 01 Basics/01Helloyoutube.js
@@ -0,0 +1,15 @@
+console.log("Hello Youtube stream")
+
+//variable declarations 
+// don't use var for variable declarations 
+let userName = "Subodh Singh"
+
+let userMobile = 9766003641
+let isLoggedIn = true
+
+// Assignment - what is difference between var and let 
+let testLet 
+var testVar
+
+// console.log(testLet)
+// console.log(testVar)
